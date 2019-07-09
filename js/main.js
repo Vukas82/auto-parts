ba35565eed4e850cbaf3ea8f20af1650a8c825f0
Vukas82 <vukas82@gmail.com>
2019-07-09 15:29:18 +0200

    home

--- js/main.js
+++ js/main.js
@@ -77,56 +77,197 @@ function myFunction() {
 //     orientation: 'right'
 // });
 
+
+
+
+
+
+
+
+
+
+
+
 // show on scroll
-var scroll = window.requestAnimationFrame ||
-    function (callback) {
-        window.setTimeout(callback, 1000 / 60)
-    };
 
-var elementsToShow = document.querySelectorAll('.show-on-scroll');
+// var scroll = window.requestAnimationFrame ||
+//     function (callback) {
+//         window.setTimeout(callback, 1000 / 60)
+//     };
+// // console.log(scroll);
 
-function loop() {
+// var elementsToShow = document.querySelectorAll('.show-on-scroll');
 
-    elementsToShow.forEach(function (element) {
-        if (isElementInViewport(element)) {
-            element.classList.add('is-visible');
-        } else {
-            element.classList.remove('is-visible');
-        }
-    });
+// // Helper function from: http://stackoverflow.com/a/7557433/274826
+// function isElementInViewport(el) {
+//     // special bonus for those using jQuery
+//     if (typeof jQuery === "function" && el instanceof jQuery) {
+//         el = el[0];
+//     }
+//     var rect = el.getBoundingClientRect();
+//     // console.log(rect)
+//     return (
+//         (rect.top <= 0 &&
+//             rect.bottom >= 0) ||
+//         (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
+//             rect.top <= (window.innerHeight || document.documentElement.clientHeight)) ||
+//         (rect.top >= 0 &&
+//             rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+//     );
+// }
 
-    scroll(loop);
-}
+// function loop() {
 
-loop();
+//     elementsToShow.forEach(function (element) {
+//         // console.log(isElementInViewport(element)) //true or false
+//         if (isElementInViewport(element)) {
 
-// Helper function from: http://stackoverflow.com/a/7557433/274826
-function isElementInViewport(el) {
-    // special bonus for those using jQuery
-    if (typeof jQuery === "function" && el instanceof jQuery) {
-        el = el[0];
-    }
-    var rect = el.getBoundingClientRect();
-    return (
-        (rect.top <= 0 &&
-            rect.bottom >= 0) ||
-        (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
-            rect.top <= (window.innerHeight || document.documentElement.clientHeight)) ||
-        (rect.top >= 0 &&
-            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
-    );
-}
+//             element.classList.add('is-visible');
+
+//         } else {
+//             element.classList.remove('is-visible');
+
+//         }
+//     });
+
+//     scroll(loop);
+// }
+
+// loop();
+// ---------------------new
+
+// function isElementInViewport(el) {
+//     //special bonus for those using jQuery
+//     if (typeof jQuery === "function" && el instanceof jQuery) {
+//         el = el[0];
+//     }
+
+//     var rect = el.getBoundingClientRect();
+
+//     return (
+//         rect.top >= 0 &&
+//         rect.left >= 0 &&
+//         rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /*or $(window).height() */
+//         rect.right <= (window.innerWidth || document.documentElement.clientWidth) /*or $(window).width() */
+//     );
+// }
+// ---------------------end new
 
 
-const callback = function (entries) {
-    entries.forEach(entry => {
-        entry.target.classList.toggle("is-visible");
-    });
-};
+// const callback = function (entries) {
+//     entries.forEach(entry => {
+//         entry.target.classList.toggle("is-visible");
+//     });
+// };
 
-const observer = new IntersectionObserver(callback);
+// const observer = new IntersectionObserver(callback);
 
-const targets = document.querySelectorAll(".show-on-scroll");
-targets.forEach(function (target) {
-    observer.observe(target);
+// const targets = document.querySelectorAll(".show-on-scroll");
+// targets.forEach(function (target) {
+//     observer.observe(target);
+// });
+// --------------------------------------------
+
+// function elementInViewport2(el) {
+//     var top = el.offsetTop;
+//     var left = el.offsetLeft;
+//     var width = el.offsetWidth;
+//     var height = el.offsetHeight;
+
+//     while(el.offsetParent) {
+//       el = el.offsetParent;
+//       top += el.offsetTop;
+//       left += el.offsetLeft;
+//     }
+
+//     return (
+//       top < (window.pageYOffset + window.innerHeight) &&
+//       left < (window.pageXOffset + window.innerWidth) &&
+//       (top + height) > window.pageYOffset &&
+//       (left + width) > window.pageXOffset
+//     );
+//   }
+
+
+//   function isElementInViewport (el) {
+//     //special bonus for those using jQuery
+//     if (typeof jQuery === "function" && el instanceof jQuery) {
+//         el = el[0];
+//     }
+
+//     var rect = el.getBoundingClientRect();
+
+//     return (
+//         rect.top >= 0 &&
+//         rect.left >= 0 &&
+//         rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /*or $(window).height() */
+//         rect.right <= (window.innerWidth || document.documentElement.clientWidth) /*or $(window).width() */
+//     );
+// }
+
+// dzoni
+
+// var waypoint = new Waypoint({
+//     element: document.getElementById('spec__offer'),
+//     handler: function (direction) {
+
+//         alert('You have scrolled to a thing')
+//         // element.addClass('is-visible')
+//     }
+// });
+// ===========================================================
+$(function () {
+    var controller = new ScrollMagic.Controller();
+
+    // var containerScene = new ScrollMagic.Scene({
+    //         triggerElement: '#container',
+    //         duration: 500
+    //     })
+    //     .setPin('#block')
+    //     .addIndicators()
+    //     .addTo(controller)
+
+    var blockTween = new TweenMax.to('#block1', .1, {
+        // backgroundColor: 'red',
+        display: 'block'
+    })
+    var containerScene = new ScrollMagic.Scene({
+            triggerElement: '#trigger1',
+        })
+        .setTween(blockTween)
+        // .addIndicators()
+        .addTo(controller);
 });
+// =============================================================
+//     .setPin('#block')
+
+// $('#block').waypoint(function (direction) {
+//     $('#block').addClass('animated');
+// }, {
+//     offset: '50%'
+// });
+
+
+
+// var scene = new ScrollMagic.Scene();
+
+// var scene = new ScrollMagic.Scene({
+//         triggerElement: '#spec__offer', // starting scene, when reaching this element
+//         duration: 400 // pin the element for a total of 400px
+//     })
+//     .setPin('#pinned'); // the element we want to pin
+
+// // Add Scene to ScrollMagic Controller
+// controller.addScene(scene);
+
+// $(document).ready(function () {
+//     console.log('radi')
+
+// $('.spec__offer').waypoint(function (direction) {
+//     console.log('radiiii')
+//     $('.spec__offer').addClass('is-visible');
+// }, {
+//     offset: '50%'
+// });
+
+// });
